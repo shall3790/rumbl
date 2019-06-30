b8c276a1df1d3c9d26105b0d1a0fb533f6f9d99f
shall3790 <shall3790@gmail.com>
2019-06-30 15:26:06 -0500

    fixed create on user controller

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -27,6 +27,7 @@ defmodule Rumbl.User do
     model
       |> changeset(params)
       |> cast(params, [:password])
+      |> validate_required(:password)
       |> validate_length(:password, min: 6, max: 100)
       |> put_pass_hash()
   end
